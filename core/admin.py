2e6a8f5f23a741c41bec7c5d80754d0d2d24fb4e
Illia <yelahinn@gmail.com>
2026-04-07 15:48:43 +0200

    feat: add City, Category, SourceRecord, Place models to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,12 @@
+from django.contrib import admin
+from .models import City, Category, SourceRecord, Place
+
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(Category)
+admin.site.register(SourceRecord)
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'city', 'latitude', 'longitude', 'sourcerecord']
