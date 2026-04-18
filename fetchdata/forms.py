28d9f8867c4c163e0ed8e4dad08007c7b49fc966
Illia <yelahinn@gmail.com>
2026-04-18 15:39:53 +0200

    chore: add new app in admin panel

---
+++ fetchdata/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from core.models import SourceRecord, City, Category
+
+class ImportPlacesForm(forms.Form):
+    pass
--- fetchdata/views.py
+++ fetchdata/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import ImportPlacesForm
+from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.contrib import admin
+
 
 # Create your views here.
+
+class ImportPlacesView(FormView):
+    admin = None
+
+    template_name = "admin/import_places.html"
+    form_class = ImportPlacesForm
+
+    # Add additional context to the page
+    def get_context_data(self, **kwargs):
+        context = {
+            "title": "Import Places",
+            "page_name": "Import Places",
+            "category_name": "Custom Pages",
+            **super().get_context_data(**kwargs),
+            **admin.site.each_context(self.request),
+        }
+        return context
+
+def custom_pages_index(request):
+    # Get list of apps from django site
+    app_list = admin.site.get_app_list(request)
+
+    context = {
+        "title": "Custom Pages administration",
+        "app_list": [app for app in app_list if app["app_label"] == "custom_pages"] # select only custom pages apps
+        }
+    return TemplateResponse(request, "admin/app_index.html", context=context)
