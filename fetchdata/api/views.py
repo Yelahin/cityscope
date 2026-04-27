0c002d50185a8744af2775cdbb8743d8a622fb3f
Illia <yelahinn@gmail.com>
2026-04-27 17:31:01 +0200

    feat: add more detailed logging to fetchdata.api.views.PlaceViewSet
    Add additional check if user povided only one coordinate. Display logs in terminal via logging package

--- fetchdata/api/views.py
+++ fetchdata/api/views.py
@@ -9,6 +9,9 @@ from django.db.models import ExpressionWrapper, Value, F
 from django.db.models.functions import Radians, ACos, Cos, Sin
 from django.db.models import FloatField
 from cityscope.settings.base import KILOMETERS
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
@@ -26,8 +29,6 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
         "opening_status",
     ]
 
-
-
     # Add distance to queryset
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -37,22 +38,36 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
         longitude = self.request.query_params.get("lon")
 
         # Return queryset without distance if user coordinates were not provided
-        if latitude is None or longitude is None:
+        if latitude is None and longitude is None:
             return queryset
-        
+
+        if latitude is None or longitude is None:
+            logging.exception("User provide only one coordinate!")
+            raise ValidationError(
+                "Both latitude and longitude should be provided!"
+            )
+
         try:
             latitude = float(latitude)
             longitude = float(longitude)
         except ValueError:
-            raise ValidationError("latitude and longitude should be floats")
-        
-        # Provide additional calculated field distance to queryset 
-        queryset = queryset.annotate(distance=ExpressionWrapper(
-            KILOMETERS * ACos(
-                Cos(Radians(F("latitude"))) * Cos(Radians(Value(latitude))) *
-                Cos(Radians(F("longitude")) - Radians(Value(longitude))) +
-                Sin(Radians(F("latitude"))) * Sin(Radians(Value(latitude)))
-            ),
-            output_field=FloatField()
-        ))
+            logging.exception("Latitude or longitude value error!")
+            raise ValidationError(
+                "Both latitude and longitude should be floats!"
+            )
+
+        # Provide additional calculated field distance to queryset
+        queryset = queryset.annotate(
+            distance=ExpressionWrapper(
+                KILOMETERS
+                * ACos(
+                    Cos(Radians(F("latitude")))
+                    * Cos(Radians(Value(latitude)))
+                    * Cos(Radians(F("longitude")) - Radians(Value(longitude)))
+                    + Sin(Radians(F("latitude")))
+                    * Sin(Radians(Value(latitude)))
+                ),
+                output_field=FloatField(),
+            )
+        )
         return queryset.order_by("distance")
