a51235a593ed0523ac207359f43c7e65a02ed52c
Illia <yelahinn@gmail.com>
2026-04-28 22:44:40 +0200

    feat: implement search behavior for name and address fields in /api/places/

--- fetchdata/api/views.py
+++ fetchdata/api/views.py
@@ -4,13 +4,14 @@ from core.models import Place
 from .serializers import PlaceSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from cityscope.settings.base import KILOMETERS
+from rest_framework.filters import SearchFilter
 from .filters import PlaceFilterSet, PlaceOrderingFilter
 
 class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, PlaceOrderingFilter]
+    filter_backends = [DjangoFilterBackend, PlaceOrderingFilter, SearchFilter]
     filterset_class = PlaceFilterSet
     ordering_fields = [
         "name",
@@ -21,3 +22,7 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
         "city",
         "distance",
         ]
+    search_fields = [
+        "name",
+        "address"
+    ]
