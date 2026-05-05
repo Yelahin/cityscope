486c7e5ae432842a42ad4f915ccae9eba22b10c4
Illia <yelahinn@gmail.com>
2026-05-05 13:50:42 +0200

    feat: add GET method with 'detail=True' for favorite places

--- fetchdata/api/views.py
+++ fetchdata/api/views.py
@@ -10,7 +10,6 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 
-
 class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
@@ -33,12 +32,16 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
     search_fields = ["name", "address"]
 
     @action(
-            detail=False, 
-            methods=["GET"], 
-            url_path="favorite", 
-            permission_classes=[IsAuthenticated], 
-            filter_backends=[DjangoFilterBackend, PlaceOrderingFilter, PlaceSearchFilter],
-            filterset_class=PlaceFilterSet,
+        detail=False,
+        methods=["GET"],
+        url_path="favorite",
+        permission_classes=[IsAuthenticated],
+        filter_backends=[
+            DjangoFilterBackend,
+            PlaceOrderingFilter,
+            PlaceSearchFilter,
+        ],
+        filterset_class=PlaceFilterSet,
     )
     def favorite_places(self, request):
         queryset = request.user.favorite_places.all()
@@ -47,13 +50,13 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
-        
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     @action(
         detail=True,
-        methods=["POST", "DELETE"],
+        methods=["GET", "POST", "DELETE"],
         permission_classes=[IsAuthenticated],
         url_path="favorite",
     )
@@ -61,17 +64,47 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
         place = get_object_or_404(Place, pk=pk)
         is_place_exists = request.user.favorite_places.filter(id=pk).exists()
 
+        if request.method == "GET":
+            if not is_place_exists:
+                return Response(
+                    data={
+                        "message": f"Place with id {pk} not found in favorite places!"
+                    },
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+            serializer = self.get_serializer(place)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
         if request.method == "POST":
             if is_place_exists:
-                return Response(data={"message": f"Place with id {pk} already in favorite places!"}, status=status.HTTP_409_CONFLICT)
-            
+                return Response(
+                    data={
+                        "message": f"Place with id {pk} already in favorite places!"
+                    },
+                    status=status.HTTP_409_CONFLICT,
+                )
+
             request.user.favorite_places.add(place)
-            return Response(data={"message": f"Place was successfully saved to favorite places!"}, status=status.HTTP_201_CREATED)
+            return Response(
+                data={
+                    "message": f"Place was successfully saved to favorite places!"
+                },
+                status=status.HTTP_201_CREATED,
+            )
 
         elif request.method == "DELETE":
             if not is_place_exists:
-                return Response(data={"message": f"Place with id {pk} is not in favorite places!"}, status=status.HTTP_400_BAD_REQUEST)
-            
+                return Response(
+                    data={
+                        "message": f"Place with id {pk} is not in favorite places!"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
             request.user.favorite_places.remove(place)
-            return Response(data={"message": f"Place was successfully removed from favorite places!"}, status=status.HTTP_200_OK)
-        
+            return Response(
+                data={
+                    "message": f"Place was successfully removed from favorite places!"
+                },
+                status=status.HTTP_200_OK,
+            )
