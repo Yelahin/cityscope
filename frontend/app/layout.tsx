ea305a39e6ea6f510bfd8058b29cc042a7b14878
Illia <yelahinn@gmail.com>
2026-06-20 12:19:58 +0200

    chore: init footer and header components

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,16 +1,15 @@
+import Header from "./ui/header";
+import Footer from "./ui/footer";
+
 export default function Layout ({ children }: {children: React.ReactNode}) {
     return (
         <html lang="en">
             <body>
-                <nav>
-                    Navigation
-                </nav>
                 <main>
+                    <Header />
                     {children}
+                    <Footer />
                 </main>
-                <footer>
-                    Footer
-                </footer>
             </body>
         </html>
     )
