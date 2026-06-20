71b3330da424064e86f8536be4d8a1fb52d25cdd
Illia <yelahinn@gmail.com>
2026-06-20 15:36:18 +0200

    chore: set up tailwindcss

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import Header from "./ui/header";
 import Footer from "./ui/footer";
 
+import "./globals.css";
+
 export default function Layout ({ children }: {children: React.ReactNode}) {
     return (
         <html lang="en">
