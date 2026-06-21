df16c4f5732c497788bace4e2f68581251808a42
Illia <yelahinn@gmail.com>
2026-06-21 19:21:58 +0200

    ui: complete header
    Make header version for mobile devices

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home () {
     return (
-        <main className="pt-14">
+        <main className="pt-header">
             <h1>Home page!</h1>
         </main>
     )
--- frontend/app/ui/header.tsx
+++ frontend/app/ui/header.tsx
@@ -1,23 +1,31 @@
+"use client";
+
 import Link from "next/link";
 import { PrimaryButtonLink } from "./PrimaryButton/PrimaryButton";
 import { SecondaryButtonLink } from "./SecondaryButton/SecondaryButton";
 import { IoLocationSharp } from "react-icons/io5";
+import BurgerMenu from "./BurgerMenu/BurgerMenu";
+import { useState} from "react";
+
 
 export default function Header () {
+    const [isBurgerMenu, setIsBurgerMenu] = useState(false);
     return (
-        <header className="flex fixed w-full h-14 items-center justify-between px-8 border-b-[0.5px] border-[rgb(35,35,35)]">
+        <header className="flex fixed w-full h-header items-center justify-between px-8 border-b-[0.5px] border-[rgb(35,35,35)]">
             <div className="flex items-center">
-                <Link href="/">
+                <Link href="/" onClick={() => setIsBurgerMenu(false)}>
                    <IoLocationSharp className="w-5.5 h-5.5 m-0 p-0 text-primary" />
                 </Link>
-                <Link href="/">
+                <Link href="/" onClick={() => setIsBurgerMenu(false)}>
                     <span className="font-bold">Cityscope</span>
                 </Link>
             </div>
-            <div className="flex items-center gap-3.5">
-                <PrimaryButtonLink href="sign-up">Sign Up</PrimaryButtonLink>
-                <SecondaryButtonLink href="login">Login</SecondaryButtonLink>
-            </div>
+            <nav className="hidden sm:flex items-center gap-3.5">
+                <PrimaryButtonLink href="/sign-up">Sign Up</PrimaryButtonLink>
+                <SecondaryButtonLink href="/login">Login</SecondaryButtonLink>
+            </nav>
+
+            <BurgerMenu isOpen={isBurgerMenu} setIsOpen={setIsBurgerMenu} />            
         </header>
     )
 }
