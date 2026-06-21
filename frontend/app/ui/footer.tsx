0bd231f60502d51118d97c0203ae928015d4a1b9
Illia <yelahinn@gmail.com>
2026-06-21 20:15:50 +0200

    ui: set up footer

--- frontend/app/ui/footer.tsx
+++ frontend/app/ui/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer () {
     return (
-        <footer>
-            <h1>Footer!</h1>
+        <footer className="flex w-full h-header items-center px-8 border-t-[0.5px] border-[rgb(35,35,35)]">
+            <p className="text-sm font-light text-mauve-600">© {new Date().getFullYear()} Illia Yelahin. All rights reserved.</p>
         </footer>
     )
 }
