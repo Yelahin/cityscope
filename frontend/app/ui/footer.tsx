6f1bbe4b169eab8c26e4109026d47ecec9d738d4
Illia <yelahinn@gmail.com>
2026-06-22 16:16:09 +0200

    ui: create separate css variable for border color

--- frontend/app/ui/footer.tsx
+++ frontend/app/ui/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer () {
     return (
-        <footer className="flex w-full h-header items-center px-8 border-t-[0.5px] border-[rgb(35,35,35)]">
+        <footer className="flex w-full h-header items-center px-8 border-t-[0.5px] border-border-primary">
             <p className="text-sm font-light text-secondary">© {new Date().getFullYear()} Illia Yelahin. All rights reserved.</p>
         </footer>
     )
--- frontend/app/ui/header.tsx
+++ frontend/app/ui/header.tsx
@@ -11,7 +11,7 @@ import { useState} from "react";
 export default function Header () {
     const [isBurgerMenu, setIsBurgerMenu] = useState(false);
     return (
-        <header className="flex fixed w-full h-header items-center justify-between px-8 border-b-[0.5px] border-[rgb(35,35,35)]">
+        <header className="flex fixed w-full h-header items-center justify-between px-8 border-b-[0.5px] border-border-primary">
             <div className="flex items-center">
                 <Link href="/" onClick={() => setIsBurgerMenu(false)}>
                    <IoLocationSharp className="w-5.5 h-5.5 m-0 p-0 text-primary" />
