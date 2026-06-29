9014bb32c3c174ed585e8b067941a90eea7ccd23
Illia <yelahinn@gmail.com>
2026-06-29 20:05:11 +0200

    feat: make backend listen to frontend requests
    Set up 'django-cors-headers' for 'http://localhost:3000' for development

--- backend/cityscope/settings/local.py
+++ backend/cityscope/settings/local.py
@@ -7,3 +7,8 @@ DEBUG = config("DJANGO_DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "backend"]
 
 INSTALLED_APPS += []
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
