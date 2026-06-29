a16bbac0d1c4f8d31884987dc950c172b1dc12e7
Illia <yelahinn@gmail.com>
2026-06-29 20:49:43 +0200

    fix: fix input div on map / more cluster logic to MapController component

--- frontend/app/ui/Map/Map.tsx
+++ frontend/app/ui/Map/Map.tsx
@@ -1,7 +1,5 @@
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
-import L from "leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet.markercluster";
-import MarkerClusterGroup from "react-leaflet-cluster";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
@@ -9,13 +7,14 @@ import Input from "../Input/Input";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import fetchApi from "@/app/lib/api/client";
+import MapController from "../MapController/MapController";
 
 interface MapProps {
     position?: [number, number];
     zoom: number;
 }
 
-interface Place {
+export interface Place {
   id: number,
   name: string,
   slug: string,
@@ -45,7 +44,7 @@ export default function Map(props: MapProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const search = searchParams.get("search");
-  const [fetchedData, setFetchedData] = useState<FetchedData>({count: 0, next: null, previous: null, results: []}); 
+  const [places, setPlaces] = useState<Place[]>([]); 
 
   function handleSubmit (): void {
     if (searchValue !== "") {
@@ -73,109 +72,34 @@ export default function Map(props: MapProps) {
           })
         )
 
-        setFetchedData({...firstPage, results: [...firstPage.results, ...results.flat()]});
+        setPlaces([...firstPage.results, ...results.flat()]);
         return;
 
       }
-      setFetchedData(firstPage);
+      setPlaces(firstPage.results);
     }
 
     fetchPlaces();
   }, [search])
 
-  function createClusterCustomIcon (cluster: L.MarkerCluster) {
-    const count = cluster.getChildCount();
-
-    const tiers: [number, string, number][] = [
-        [10000, "#BE1A1A", 75],
-        [5000, "#5E244E", 70],
-        [2000, "#D0311E", 67.5],
-        [1000, "#4274D9", 65],
-        [750, "#659287", 62.5],
-        [500, "#FF6A1C", 60],
-        [250, "#FF97D0", 57.5],
-        [200, "#0D530E", 55],
-        [150, "#E4D329", 52.5],
-        [100, "#95CCDD", 50],
-        [75, "#723EC3", 47.5],
-        [50, "#1E93AB", 45],
-        [40, "#FF0066", 42.5],
-        [30, "#253900", 40],
-        [20, "#40534C", 37.5],
-        [10, "#8494FF", 35],
-        [5, "#DF6D2D", 32.5],
-        [3, "#FFEDFA", 30],
-    ];
-
-    const [, color, size] = tiers.find(([threshold]) => count > threshold) ?? [, "#9929EA", 25]
-
-
-    return L.divIcon({
-      html: `
-        <div style="background-color: ${color}80">
-          <span style="background-color: ${color}99; color: black;">${cluster.getChildCount()}</span>
-        </div>
-      `,
-      className: "custom-marker-cluster",
-      iconSize: L.point(size, size, true),
-    });
-  }
-
   return (
     <>
-      <div className="fixed flex items-center z-1000 top-header right-0 w-full h-header sm:px-30 px-15 transition-all duration-300">
+      <div className="fixed flex items-center z-1000 top-header right-0 w-full h-header sm:px-30 px-15 pointer-events-none transition-all duration-300">
           <Input 
+            className="pointer-events-auto"
             placeholder="Search..." 
             value={searchValue} 
             onChange={(e) => setSearchValue(e.target.value) }
             onSubmit={handleSubmit}
           />
       </div>
-      {}
       <MapContainer center={position ?? defaultPosition} minZoom={2} maxBounds={[[-90, -200], [90, 200]]} maxBoundsViscosity={1} zoom={zoom} className="h-main-content w-full">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
       />
-
-        {position && <Marker position={position} />}
-        <MarkerClusterGroup
-          iconCreateFunction={createClusterCustomIcon}
-          maxClusterRadius={100}
-          spiderfyOnMaxZoom={true}
-          polygonOptions={{
-            fillColor: '#ffffff',
-            color: '#f00800',
-            weight: 5,
-            opacity: 0,
-            fillOpacity: 0.8,
-          }}
-          showCoverageOnHover={false}
-        >
-          {fetchedData.results && fetchedData.results.map((place) => {
-            const address = <p>Address: {place.address}</p>
-            const distance = <p>Distance: {place?.distance}</p>
-            const rating = <p>Rating: {place?.rating}</p>
-            const price_level = <p>Price Level: {place?.price_level}</p>
-
-
-            return (
-              <Marker key={place.id} position={[place.latitude, place.longitude]}>
-                <Popup>
-                  <p>Name: {place.name}</p>
-                  {place.address && address}
-                  <p>City: {place.city.name}</p>
-                  <p>Category: {place.category.name}</p>
-                  {place?.distance && distance}
-                  {place?.rating && rating}
-                  {place?.price_level && price_level}
-                  <p>Latitude: {place.latitude}</p>
-                  <p>Longitude: {place.longitude}</p>
-                </Popup>
-              </Marker>
-            )
-          })}
-        </MarkerClusterGroup>
+      {position && <Marker position={position} />}
+       <MapController places={places} />
       </MapContainer>
     </>
   )
