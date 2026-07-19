9054a25557400b9bf8e755f0f88589cfad2b78f2
Illia <yelahinn@gmail.com>
2026-07-19 12:08:54 +0200

    ui: make FormContainer component behave better with smaller forms

--- frontend/app/ui/FormContainer.tsx
+++ frontend/app/ui/FormContainer.tsx
@@ -1,6 +1,6 @@
 export default function FormContainer ({children, className}: {children: React.ReactNode, className?: string}) {
     return (
-        <div className={`flex flex-col gap-4 max-w-100 min-h-65 w-full bg-neutral-900 rounded-2xl p-5 border border-primary
+        <div className={`flex flex-col gap-4 max-w-100 min-h-5 w-full bg-neutral-900 rounded-2xl p-5 border border-primary
         ${className}`}>
             {children}
         </div>
