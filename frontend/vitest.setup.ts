bafa8387433fcb4b1a1c5e97b65ff6dc7eefc6f8
Illia <yelahinn@gmail.com>
2026-07-28 22:53:41 +0200

    chore: enhance configuration files for frontend test

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -1,5 +1,10 @@
-import { afterEach } from "vitest";
+import { afterEach, beforeEach } from "vitest";
 import { cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/vitest';
+
+beforeEach(() => {
+    vi.clearAllMocks();
+});
 
 afterEach(() => {
     cleanup();
