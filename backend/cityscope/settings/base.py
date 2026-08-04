6703cef73025e97e9018f3c96d1dc28d767461a1
Illia <yelahinn@gmail.com>
2026-08-04 19:24:21 +0200

    fix: change celery settings from broker to redis

--- backend/cityscope/settings/base.py
+++ backend/cityscope/settings/base.py
@@ -86,8 +86,8 @@ CACHES = {
     }
 }
 
-CELERY_BROKER_URL = "redis://broker:6379/0"
-CELERY_RESULT_BACKEND = "redis://broker:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 CELERY_TIMEZONE = "Europe/Paris"
 
