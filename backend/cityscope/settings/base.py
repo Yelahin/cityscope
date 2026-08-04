82fc012cb2b7177a32c43d701c6e43d69f6090bf
Illia <yelahinn@gmail.com>
2026-08-04 19:35:47 +0200

    chore: set redis host for container and github actions

--- backend/cityscope/settings/base.py
+++ backend/cityscope/settings/base.py
@@ -79,15 +79,17 @@ DATABASES = {
     }
 }
 
+REDIS_HOST = config("REDIS_HOST", default="localhost")
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
+        "LOCATION": f"redis://{REDIS_HOST}:6379/1",
     }
 }
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = f"redis://{REDIS_HOST}:6379/0"
+CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:6379/0"
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 CELERY_TIMEZONE = "Europe/Paris"
 
